bd4e371d3e16455e2cb589d2e67db2e0dbbe3f64
Koral <koral@Korals-MacBook-Air.local>
2023-07-31 13:49:32 +0300

    pdf link control

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function() {
     var downloadLink = document.getElementById('downloadLink');
-    downloadLink.href = "https://drive.google.com/uc?id=1VGsW9UYs8X73WhEuz6JSuhFK8sccuS_6";
+    var pdfLink = "https://drive.google.com/uc?id=1VGsW9UYs8X73WhEuz6JSuhFK8sccuS_6";
+
+    if (pdfLink) {
+        downloadLink.href = pdfLink;
+    } else {
+        downloadLink.textContent = "There is not a sharable pdf currently";
+        downloadLink.href = "#";
+        downloadLink.style.pointerEvents = "none"; // Disables the link
+    }
 };
