28272429ab14f946fff62c151f69d65f4d7a9277
Koral <koral@Korals-MacBook-Air.local>
2023-07-31 12:19:39 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    var downloadLink = document.getElementById('downloadLink');
+    downloadLink.href = "https://drive.google.com/uc?id=1VGsW9UYs8X73WhEuz6JSuhFK8sccuS_6";
+};
