ed47dfe438e0da8dd6dd08f47a736f3edbf334d5
Koral <koral@Korals-MacBook-Air.local>
2023-09-11 17:03:02 +0300

    a

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = function() {
 
     if (pdfLink) {
         downloadLink.href = pdfLink;
+        downloadLink.target = "_blank"; // This will open the PDF in a new tab
     } else {
         downloadLink.textContent = "There is not a sharable pdf currently";
         downloadLink.href = "#";
